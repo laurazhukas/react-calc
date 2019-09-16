04b0c1bff02505a84e968ea9f0916e86c78b7349
laurazhukas <lzhukas@hotmail.ca>
2019-09-16 09:39:56 -0400

    updated components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './styles/styles.css';
-const app = () => <div className ="app"></div>;
+import Calculator from './layout/Calculator/Calculator'; //first letter casting (import w upper case)
+
+const app = () => (
+<div className ="app">
+    <Calculator />
+</div>
+);
+
+
 export default app;
 
 //https://blog.usejournal.com/everything-react-first-app-188b33a880ca
