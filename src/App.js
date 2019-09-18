fd07d8215d13949927e5ab89ed8b5b391f562253
laurazhukas <lzhukas@hotmail.ca>
2019-09-18 16:01:02 -0400

    final updates

--- src/App.js
+++ src/App.js
@@ -10,28 +10,3 @@ const app = () => (
 
 
 export default app;
-
-//https://blog.usejournal.com/everything-react-first-app-188b33a880ca
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
